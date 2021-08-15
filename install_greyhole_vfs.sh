a5a0efd3dd6856919198c8fa345162248a3aba98
Guillaume Boudreau <guillaume@pommepause.com>
2021-08-15 07:32:40 -0400

    When looking for an already compiled VFS module on the web server, make sure it is the right one for the current arch (x86_64, aarch64, armv7, ...)

--- install_greyhole_vfs.sh
+++ install_greyhole_vfs.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+arch=$(uname -m)
 version=$(/usr/sbin/smbd --version | awk '{print $2}' | awk -F'-' '{print $1}')
 V=$(echo "${version}" | awk -F'.' '{print $1$2}')
-curl -sLO "https://www.greyhole.net/releases/greyhole-samba${V}-alpine.zip"
+vfs_file="greyhole-samba${V}-alpine-${arch}.zip"
+curl -sLO "https://www.greyhole.net/releases/$vfs_file"
 
-if file "greyhole-samba${V}-alpine.zip" | grep HTML >/dev/null; then
+if file "$vfs_file" | grep HTML >/dev/null; then
+    rm -f "$vfs_file"
     echo "Compiling new greyhole.so VFS using Samba source..."
     if ! TERM=xterm GREYHOLE_INSTALL_DIR=$(pwd) bash ./build_vfs.sh current; then
-        rm -f "greyhole-samba${V}-alpine.zip"
         exit 1
     fi
     cp -Lr vfs-build/samba-*/bin/shared ./samba-bin-shared
@@ -18,12 +20,19 @@ if file "greyhole-samba${V}-alpine.zip" | grep HTML >/dev/null; then
     mv greyhole-samba*.so "vfs-build/$(ls -1 vfs-build | grep samba-)/"
     (cd vfs-build/ ; rm -rf .git* .lock-wscript .testr.conf .ycm_extra_conf.py)
     (cd "vfs-build/$(ls -1 vfs-build | grep samba-)/" ; rm -rf .git* .lock-wscript .testr.conf .ycm_extra_conf.py)
+    # To copy to web server:
+    apk add zip >/dev/null
+    zip -r "$vfs_file" vfs-build/
+    apk del zip >/dev/null
+    echo
+    echo "$vfs_file file created. You can copy it to the host using: sudo docker cp container_name_here:$(pwd)/$vfs_file ."
+    echo
 else
-    echo "Using pre-compiled greyhole.so VFS from greyhole.net/releases/greyhole-samba${V}-alpine.zip"
+    echo "Using pre-compiled greyhole.so VFS from greyhole.net/releases/$vfs_file"
     rm -rf vfs-build
-    unzip "greyhole-samba${V}-alpine.zip" >/dev/null
+    unzip "$vfs_file" >/dev/null
     ln -s "$(pwd)"/vfs-build/samba-*/greyhole-samba"${V}".so /usr/lib/samba/vfs/greyhole.so
+    rm -f "$vfs_file"
 fi
 
-rm -f "greyhole-samba${V}-alpine.zip"
 rm -rf samba-module
